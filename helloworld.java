d38768cf951d8b89e8f633ac6b9a06cc29873aca
Saikrishna Vallala <143808001+aka-Saikrishna@users.noreply.github.com>
2024-01-10 22:13:17 +0530

    Helloworld

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+
+public class helloworld {
+	public static void main(String args[]) {
+		System.out.println("HelloWorld");
+		System.out.print("HelloWorld");
+		System.out.println("HelloWorld");
+//		if we use println, java will put a newline at the end
+	}
+}
