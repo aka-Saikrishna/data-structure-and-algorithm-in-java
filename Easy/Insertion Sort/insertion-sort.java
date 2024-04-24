8bc13f7333156b1186901df2fcdc724bed53363e
Saikrishna Vallala <143808001+aka-Saikrishna@users.noreply.github.com>
2024-04-24 22:44:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Insertion Sort/insertion-sort.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.Math;
+
+class Sorting
+{
+	
+	static void printArray(int arr[],int size)
+	{
+		int i;
+		for(i=0;i<size;i++)
+		System.out.print(arr[i]+" ");
+	    System.out.println();
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t= sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+		
+			for(int i=0;i<n;i++)
+			a[i]= sc.nextInt();
+			
+			 new Solution().insertionSort(a,n);
+			 printArray(a,n);
+			
+		t--;
+		}
+		
+	}
+}
+// } Driver Code Ends
+
+class Solution
+{
+  static void insert(int arr[],int i)
+  {
+      int k=i;
+       for(int j=i+1;j>0 && arr[k]>arr[j] ;j--)
+       {
+           int temp=arr[k];
+           arr[k]=arr[j];
+           arr[j]=temp;
+           k--;
+       }
+  }
+
+  public void insertionSort(int arr[], int n)
+  {
+      for(int i=0;i<n-1;i++)
+      {
+          insert(arr,i);
+      }
+  }
+}
