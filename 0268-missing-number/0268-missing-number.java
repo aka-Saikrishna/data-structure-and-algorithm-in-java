59bc31882cec13b411385f4711ba533c10bd1d63
Saikrishna Vallala <143808001+aka-Saikrishna@users.noreply.github.com>
2024-04-26 22:40:30 +0530

    Time: 0 ms (100%), Space: 45.3 MB (38.49%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for(int i=0; i<n; i++) 
+            sum += nums[i];
+        int sumOfNNatural = n*(n+1)/2;
+        return sumOfNNatural - sum;
+    }
+}
