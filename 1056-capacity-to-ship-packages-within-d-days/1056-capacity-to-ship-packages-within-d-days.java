165407a83d50d4a5204bfd112e47ed015d83c0c0
Saikrishna Vallala <143808001+aka-Saikrishna@users.noreply.github.com>
2024-05-23 23:32:03 +0530

    Time: 10 ms (91.11%), Space: 46.7 MB (86.08%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,27 +1,40 @@
+
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int maxWeight = -1, totalWeight = 0;
-        for (int weight : weights) {
-            maxWeight = Math.max(maxWeight, weight);
-            totalWeight += weight;
+        int left = 0, right = 0;
+        
+        // Find the minimum and maximum capacity required for the ship
+        for (int w : weights) {
+            left = Math.max(left, w); // minimum capacity should be able to handle the heaviest package
+            right += w; // maximum capacity should be able to handle all packages
         }
-        int left = maxWeight, right = totalWeight;
+        
+        // Use binary search to find the least weight capacity required for the ship
         while (left < right) {
             int mid = (left + right) / 2;
-            int daysNeeded = 1, currWeight = 0;
-            for (int weight : weights) {
-                if (currWeight + weight > mid) {
-                    daysNeeded++;
-                    currWeight = 0;
+            int need = 1, cur = 0;
+            
+            // Count the number of days required to ship all packages with current mid capacity
+            for (int w : weights) {
+                if (cur + w > mid) { // current capacity can't handle the current package
+                    need++; // increment the number of days required
+                    cur = 0; // reset the current capacity
                 }
-                currWeight += weight;
+                cur += w; // add the current package to the current capacity
             }
-            if (daysNeeded > days) {
+            
+            // If the number of days required is greater than the given number of days, increase the weight capacity
+            if (need > days) {
                 left = mid + 1;
-            } else {
+            } 
+            // If the number of days required is less than or equal to the given number of days, decrease the weight capacity
+            else {
                 right = mid;
             }
         }
+        
+        // Return the least weight capacity required to ship all packages within the given number of days
         return left;
     }
+
 }
