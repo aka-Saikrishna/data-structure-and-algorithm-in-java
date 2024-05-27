eb131f00f01b4f878ed422b3565e540cb7a9e912
Saikrishna Vallala <143808001+aka-Saikrishna@users.noreply.github.com>
2024-05-27 23:31:44 +0530

    Time: 10 ms (62.21%), Space: 42.3 MB (61.44%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,19 +1,31 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap< Character , Character > map = new HashMap <>();
-        for(int i = 0 ; i < s.length(); i++){
-            if( !map.containsKey(s.charAt(i)) ){
-                if( map.containsValue(t.charAt(i)) ){
+        
+        HashMap<Character, Character> mapS2T = new HashMap<>();
+        HashMap<Character, Character> mapT2S = new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+            
+            // Check if there's a mapping for charS in mapS2T and if it maps to the same character in t
+            if (mapS2T.containsKey(charS)) {
+                if (mapS2T.get(charS) != charT) {
                     return false;
-                }else{
-                    map.put(s.charAt(i),t.charAt(i));
                 }
-            }else{
-                if( map.get(s.charAt(i)) != t.charAt(i) ){
+            } else { // If no mapping exists, check if charT is already mapped to some other character in mapT2S
+                if (mapT2S.containsKey(charT)) {
                     return false;
                 }
+                
+                // Create new mapping since it's valid
+                mapS2T.put(charS, charT);
+                mapT2S.put(charT, charS);
             }
         }
+        
         return true;
     }
+    
+    
 }
