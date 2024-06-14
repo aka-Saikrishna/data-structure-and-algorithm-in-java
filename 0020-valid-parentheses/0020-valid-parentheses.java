1d518ab0719d3174c9f7d679a045458760d53f5c
Saikrishna Vallala <143808001+aka-Saikrishna@users.noreply.github.com>
2024-06-14 23:35:43 +0530

    Time: 2 ms (54.84%), Space: 41.3 MB (55.9%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> mapping = new HashMap<>();
+        mapping.put(')', '(');
+        mapping.put('}', '{');
+        mapping.put(']', '[');
+
+        for (char c : s.toCharArray()) {
+            if (mapping.containsValue(c)) {
+                stack.push(c);
+            } else if (mapping.containsKey(c)) {
+                if (stack.isEmpty() || mapping.get(c) != stack.pop()) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();        
+    }
+}
